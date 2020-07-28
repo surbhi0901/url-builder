dc2bf63db977ede429c638829cb128203b549d33
surbhi0901 <55763820+surbhi0901@users.noreply.github.com>
2020-07-28 16:29:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementById("final-url").value = document.getElementById('url').value + "#src=" + document.getElementById("source").value + "&team=" + document.getElementById("team").value;
+}
