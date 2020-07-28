bd209a8aa7b0a28cb2a488cd29b28715fed38c6b
surbhi0901 <55763820+surbhi0901@users.noreply.github.com>
2020-07-28 16:25:42 +0530

    Update script.js

--- project/script.js
+++ project/script.js
@@ -1,1 +1,4 @@
 //Write your JavaScript Code here
+function myFunction() {
+    document.getElementById("final-url").value = document.getElementById('url').value + "#src=" + document.getElementById("source").value + "&team=" + document.getElementById("team").value;
+}
